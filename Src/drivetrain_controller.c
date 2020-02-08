2dd6e5204c9926346789d48b9d0e5e42ea493ebe
DeanKamen <messn036@umn.edu>
2020-02-07 18:34:52 -0600

    added odometry algorithm

--- Src/drivetrain_controller.c
+++ Src/drivetrain_controller.c
@@ -10,16 +10,18 @@
 #include <task.h>
 #include "VESC.h"
 #include "stdlib.h"
+#include "math.h"
 
-// I am telling the motor controllers to implement a twist message until I get
-// another twist message
-double new_speed_left = 0;
-double new_speed_right = 0;
+//I am telling the motor controllers to implement a twist message until I get another twist message
+F32 new_speed_left = 0;
+F32 new_speed_right = 0;
+F32 theta = 0; //keeps tracks of robot angle, init facing 0 degrees on the top down xy coordinate plane
+F32 x = 0, y = 0; //absolute position of the robot, starts at (0,0)
 
 void drivetrain_move(rmc_can_msg msg) {
-  // Because of the mask we only get messages that have our ID
-  int32_t cmd_speed = 0;  // in mm/s
-  int32_t cmd_angV = 0;
+    // Because of the mask we only get messages that have our ID
+    int32_t cmd_speed = 0; //in mm/s
+    int32_t cmd_angV = 0; //in mrad/s
 
 //    if(msg.id & 0xFF != DRIVETRAIN_SYS_ID)
 //    {
@@ -59,13 +61,38 @@ void drivetrain_loop(void) {
         DRIVE_LOOP_MS * portTICK_RATE_MS);  // Not sure what this does yet,
     // min 1hz refresh rate
 
-    vesc_set_rpm(blm, new_speed_left);
-    vesc_set_rpm(flm, new_speed_left);
-    vesc_set_rpm(brm, new_speed_right);
-    vesc_set_rpm(frm, new_speed_right);
+        F32 rpm_right = new_speed_right/(RADIUS*2*3.14);
+        F32 rpm_left = new_speed_left/(RADIUS*2*3.14);
+        vesc_set_rpm(blm, rpm_left);
+        vesc_set_rpm(flm, rpm_left);
+        vesc_set_rpm(brm, rpm_right);
+        vesc_set_rpm(frm, rpm_right);
 
         //now publish some optometry data
         //idk what to send.
+        //keep track of the position estimate, you start at 0,0, publish the position estimate every loop.
+
+        //I am using this model
+        //https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-186-mobile-autonomous-systems-laboratory-january-iap-2005/study-materials/odomtutorial.pdf
+
+        //distance traveled = wheel radius * rpm * loop length, this will be done for each side
+        //D_center= (Dleft+Dright) /2
+        //Angle change from orgin, theta= dright-dleft/(WIDTH)
+        //direction = angle change from origin+ previous direction
+        //x_new = x_old +d_center*cos(theta)
+        //y_new = y_old +d_center*sin(theta)
+        F32 speed_right = vesc_get_rpm(brm);
+        F32 speed_left  = vesc_get_rpm(blm);
+
+        F32 d_left= speed_left*(DRIVE_LOOP_MS/1000); //distance traveled by the left side, in mm
+        F32 d_right= speed_right*(DRIVE_LOOP_MS/1000); //distance traveled by the right side, in mm
+        F32 d_center = (d_left+d_right)/2;
+        //https://en.wikipedia.org/wiki/Polar_coordinate_system
+        F32 phi = (d_right-d_left)/WIDTH; // how much the robot has turned this loop
+        theta = phi+theta; // theta can be negative
+        //no need to map phi from 0 to 2pi, cos function will do that for me
+        x = x + d_center*cos(phi);
+        y = y + d_center*sin(phi);
     }
 
 //give vescs rpm command
